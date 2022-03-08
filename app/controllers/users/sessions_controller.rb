ef84c25cc590f346bde898ba531c6a58c3fa3eb6
unknown <dortizchaves@outlook.com>
2022-03-08 11:39:32 -0600

    adding back signpath in session controller

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,24 +3,18 @@ class Users::SessionsController < Devise::SessionsController
       new_user_session_path
     end
   
-    #def after_sign_in_path_for(resource_or_scope)
-      #users.update(signInCount: (signInCount+1))
+    def after_sign_in_path_for(resource_or_scope)
+      users.update(signInCount: (signInCount+1))
   
-      #if current_user.signInCount == 1
-        #stored_location_for(resource_or_scope) || edit_user_session_path
-      #elsif current_user.admin == true
-        #stored_location_for(resource_or_scope) || admin_root_path
-      #else
-        #stored_location_for(resource_or_scope) || root_path
-      #end
-    #end
-
-<<<<<<< HEAD
-=======
-    def edit;  
-      #puts("hecker")
+      if current_user.signInCount == 1
+        stored_location_for(resource_or_scope) || edit_user_session_path
+      elsif current_user.admin == true
+        stored_location_for(resource_or_scope) || admin_root_path
+      else
+        stored_location_for(resource_or_scope) || root_path
+      end
     end
->>>>>>> 93e4be2b09798d36652c5079195df8bb599f239a
+
 
     def update
 
