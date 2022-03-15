f33dd5df0d47e7ca6a21e0ef808c027b79a66cc1
unknown <dortizchaves@outlook.com>
2022-03-15 14:03:31 -0500

    UI updating for nearly every page and added some restricted pages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,9 @@
 class HomeController < ApplicationController
 
 
-  def indexs
+  def index
+    if request.path == "/home/adminDash"
+      redirect_to root_path unless current_user.admin?
+    end
   end
 end
--- app/controllers/poin_events_controller.rb
+++ app/controllers/poin_events_controller.rb
@@ -11,11 +11,14 @@ class PoinEventsController < ApplicationController
 
   # GET /poin_events/new
   def new
+    redirect_to poin_events_path unless current_user.admin?
     @poin_event = PoinEvent.new
   end
 
   # GET /poin_events/1/edit
-  def edit; end
+  def edit 
+    redirect_to poin_events_path unless current_user.admin?
+  end
 
   # POST /poin_events or /poin_events.json
   def create
@@ -34,6 +37,7 @@ class PoinEventsController < ApplicationController
 
   # PATCH/PUT /poin_events/1 or /poin_events/1.json
   def update
+    redirect_to poin_events_path unless current_user.admin?
     respond_to do |format|
       if @poin_event.update(poin_event_params)
         format.html { redirect_to poin_event_url(@poin_event), notice: 'Poin event was successfully updated.' }
@@ -47,6 +51,7 @@ class PoinEventsController < ApplicationController
 
   # DELETE /poin_events/1 or /poin_events/1.json
   def destroy
+    redirect_to poin_events_path unless current_user.admin?
     @poin_event.destroy
 
     respond_to do |format|
