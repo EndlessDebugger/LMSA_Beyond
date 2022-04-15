223dacff1c74dbb54e3ffc453b6f36c68cea04cd
unknown <dortizchaves@outlook.com>
2022-04-15 01:51:08 -0500

    css changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,24 @@
 Rails.application.routes.draw do
-  get 'myhistory/index'
-  get 'myhistory/points_leaderboard'
-  get 'home/info'
-  get 'home/adminhelp'
-  get 'home/userhelp'
-  resources :helps
-  resources :announcements
   root to: 'home#index'
-  post '/sign_in_event', to: 'events#sign_in_event'
-  get '/home/adminDash' => 'home#adminDash', :as => :admin_root
+
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', sessions: 'users/sessions' }
   devise_scope :user do
     get 'users/edit', to: 'users/sessions#edit', as: :edit_user_session
     patch 'users/edit', to: 'users/sessions#update'
     get 'users/sign_in', to: 'users/sessions#new', as: :new_user_session
     get 'users/sign_out', to: 'users/sessions#destroy', as: :destroy_user_session
-    get 'referrals/admin', as: :admin_approve
+    get 'officers/approval_list', to:'referrals#admin', as: :admin_approve
+    get 'officers' => 'home#adminDash', :as => :admin_root
+    get 'officers/help'=> 'home#adminhelp', :as => :home_adminhelp
+    get 'users/help'=>'home#userhelp', :as => :home_userhelp
+    get 'info'=>'home#info', :as => :home_info
     get 'myhistory/index'
-    get 'myhistory/points_leaderboard'
     post 'users/dev/:id' => 'users#dev', :as => 'dev'
     post 'users/nodev/:id' => 'users#nodev', :as => 'nodev'
     post '/create_new_admin', to: 'users#create_new_admin'
+    post '/sign_in_event', to: 'events#sign_in_event'
+    get 'leaderboard'=>'myhistory#points_leaderboard', :as =>:myhistory_points_leaderboard
+    get 'point_history' => 'poin_events#index', :as => :poin_events
   end
 
   resources :home
@@ -32,5 +30,7 @@ Rails.application.routes.draw do
   resources :users
   resources :sessions
   resources :signups # plural???
+  resources :helps
+  resources :announcements
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
