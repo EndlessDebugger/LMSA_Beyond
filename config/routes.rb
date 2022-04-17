32603928c0db19d31a4b0ddb89de6c5cefd69179
unknown <dortizchaves@outlook.com>
2022-04-17 12:59:47 -0500

    fixed point award issue

--- config/routes.rb
+++ config/routes.rb
@@ -7,22 +7,28 @@ Rails.application.routes.draw do
     patch 'users/edit', to: 'users/sessions#update'
     get 'users/sign_in', to: 'users/sessions#new', as: :new_user_session
     get 'users/sign_out', to: 'users/sessions#destroy', as: :destroy_user_session
+
     get 'officers/approval_list', to:'referrals#admin', as: :admin_approve
     get 'officers' => 'home#adminDash', :as => :admin_root
     get 'officers/help'=> 'home#adminhelp', :as => :home_adminhelp
     get 'users/help'=>'home#userhelp', :as => :home_userhelp
     get 'info'=>'home#info', :as => :home_info
     get 'myhistory/index'
+
     post 'users/dev/:id' => 'users#dev', :as => 'dev'
     post 'users/nodev/:id' => 'users#nodev', :as => 'nodev'
     post '/create_new_admin', to: 'users#create_new_admin'
     post '/sign_in_event', to: 'events#sign_in_event'
+
     get 'leaderboard'=>'myhistory#points_leaderboard', :as =>:myhistory_points_leaderboard
-    get 'point_history' => 'poin_events#index', :as => :poin_events
+
+    # get 'point_history' => 'poin_events#index', :as => :poin_events
+    # post 'point_history' => 'poin_events#index', :as => :poin_events
+    resources :poin_events, :path=> :point_history
   end
 
   resources :home
-  resources :poin_events
+  # resources :poin_events
   resources :referrals
   resources :events
   resources :categories
