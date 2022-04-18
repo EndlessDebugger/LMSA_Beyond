ad7891f0e0344cedebae3a279992980a7e51d258
unknown <dortizchaves@outlook.com>
2022-04-18 18:44:36 -0500

    updated points table migration

--- db/migrate/20220417190034_create_points.rb
+++ db/migrate/20220417190034_create_points.rb
@@ -1,11 +1,11 @@
 class CreatePoints < ActiveRecord::Migration[6.1]
   def change
     create_table :points do |t|
-      t.integer :referrals
-      t.integer :prof_referrals
-      t.integer :gen_event
-      t.integer :fund_event
-      t.string :vol_event
+      t.integer :referrals, :default => 1
+      t.integer :prof_referrals, :default => 4
+      t.integer :gen_event, :default => 4
+      t.integer :fund_event, :default => 3
+      t.string :vol_event, :default => 3
 
       t.timestamps
     end
