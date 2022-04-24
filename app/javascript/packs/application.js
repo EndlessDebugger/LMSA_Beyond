c050e2925f64b45352126f9f7779101a9493c5bd
unknown <dortizchaves@outlook.com>
2022-04-24 01:51:24 -0500

    fixed text editor rendering and added css to login page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -22,6 +22,8 @@ import { Calendar } from '@fullcalendar/core'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid';
 import listPlugin from '@fullcalendar/list';
+import "trix";
+import "@rails/actiontext";
 
 
 import "trix"
